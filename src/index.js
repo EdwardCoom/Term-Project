4638cfe0bc26bd6724538ee15b237b83df3410ec
Daniel-Cunningham02 <danrcunn@ut.utm.edu>
2023-10-18 14:03:01 -0500

    Modularized the code. Initialization is in its own file now. Will be moving renderer and its settings to its own file as well at some point

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import * as THREE from 'three';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import {createChessEnvironment} from './initalization.js';
 window.onload = function init()
 {
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.needsUpdate = true;
+    renderer.shadowMap.type = THREE.VSMShadowMap;
     document.body.appendChild(renderer.domElement);
 
     const camera = new THREE.PerspectiveCamera(45, window.innerWidth/ window.innerHeight, 1 , 500);
@@ -17,42 +21,15 @@ window.onload = function init()
     const scene = new THREE.Scene();
     
     const loader = new GLTFLoader();
-
-    loader.load( './models/chess.glb', (gltf) => {
-        var board = gltf.scene;
-        board.scale.set(0.25, 0.25, 0.25);
-        board.rotation.x = 0
-        board.position.set(0, 0, 0);
-        scene.add(board);
-    }, undefined, (error) => {
-        console.error(error);
-    });
-
-    loader.load('./models/StylishDesk.glb', (gltf) => {
-        var table = gltf.scene;
-        table.scale.set(3, 0, 2);
-        table.rotation.x = 0;
-        table.position.set(0, -0.22, 0);
-        scene.add(table);
-    }, undefined, (error) => {
-        console.error(error);
-    })
     
-    loader.load('./models/white_pawn.glb', (gltf) => {
-        var pawn = gltf.scene;
-        pawn.scale.set(0.25, 0.25, 0.25);
-        pawn.position.set(0, 0, 0);
-        scene.add(pawn);
-    }, undefined, (error) => {
-        console.error(error);
-    });
+    createChessEnvironment(scene)
 
     const AmbLight = new THREE.AmbientLight(0xffffff, 1);
     AmbLight.position.set(100);
     scene.add(AmbLight)
     
-    const light = new THREE.SpotLight(0xE8DB9F, 10);
-    light.position.set(0, 2, 0);
+    const light = new THREE.SpotLight(0xE8DB9F, 15);
+    light.position.set(0, 3, 0);
     light.target.position.set(0, 0, 0)
     scene.add(light)
     renderer.setClearColor(new THREE.Color(0xffffff), 1.0)
@@ -64,4 +41,11 @@ window.onload = function init()
     }
     animate();
 
+    window.onresize = () => {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.aspect = window.innerWidth / window.innerHeight;
+    }
 }
+
+
+
---
+++ src/initalization.js
@@ -0,0 +1,65 @@
+/* Each square on the board seems to be 0.4 in length and width.
+ * Later, the algorithms below will use this to determine the place
+ * of the piece on the board
+ */
+import * as THREE from 'three';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js'
+
+const pieceScale = 0.25
+const squareSize = 0.4
+export function createChessEnvironment(scene) {
+  var loader = new GLTFLoader();
+  createBoard(scene, loader)
+  createTable(scene, loader)
+  createWhitePieces(scene, loader)
+  createBlackPieces(scene, loader)
+}
+
+
+function createBoard(scene, loader) {
+  loader.load('./models/chess.glb', (gltf) => {
+      var board = gltf.scene;
+      board.scale.set(0.25, 0.25, 0.25)
+      board.position.set(0, 0, 0)
+      scene.add(board)
+  }, undefined, (error) => {
+    console.error(error)
+  })
+}
+
+function createTable(scene, loader) {
+  loader.load('./models/StylishDesk.glb', (gltf) => {
+    var table = gltf.scene;
+    table.scale.set(3, 0, 2);
+    table.position.set(0, -0.22, 0);
+    scene.add(table)
+  }, undefined, (error) => {
+    console.error(error);
+  })
+}
+
+function createWhitePieces(scene, loader) {
+  for (let i = 0; i < 8; i++) {
+    loader.load('./models/white_pawn.glb', (gltf) => {
+        var pawn = gltf.scene;
+        pawn.scale.set(pieceScale, pieceScale, pieceScale);
+        pawn.position.set(1, 0, 1.4 -(i * squareSize));
+        scene.add(pawn)
+    }, undefined, (error) => {
+      console.error(error)
+    })
+  }
+}
+
+function createBlackPieces(scene, loader) {
+  for(let i = 0; i < 8; i++) {
+    loader.load('./models/white_pawn.glb', (gltf) => {
+      var pawn = gltf.scene;
+      pawn.scale.set(pieceScale, pieceScale, pieceScale);
+      pawn.position.set(-1, 0, 1.4 - (i * squareSize));
+      scene.add(pawn)
+    }, undefined, (error) => {
+      console.error(error);
+    })
+  }
+}

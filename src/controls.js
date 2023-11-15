bbb971ddf66864808e5ce690a97a819422647ced
Zachary Rose <zacerose@ut.utm.edu>
2023-11-14 23:21:14 -0600

    Added bounds for each square, so now the piece automatically goes to the center of the clicked square. No protection against overlapping at this time, and be careful not to reposition any other models in the room

--- src/controls.js
+++ src/controls.js
@@ -4,6 +4,16 @@ import { Object3D } from 'three';
 var firstClick = false;
 var pieceClicked = NaN;
 var pieceMaterial = 0;
+var boardSpaceCoordinates = [
+    [new THREE.Vector3(1.4, 0, 1.4), new THREE.Vector3(1.0, 0, 1.4), new THREE.Vector3(0.6, 0, 1.4), new THREE.Vector3(0.2, 0, 1.4), new THREE.Vector3(-0.2, 0, 1.4), new THREE.Vector3(-0.6, 0, 1.4), new THREE.Vector3(-1.0, 0, 1.4), new THREE.Vector3(-1.4, 0, 1.4)],
+    [new THREE.Vector3(1.4, 0, 1.0), new THREE.Vector3(1.0, 0, 1.0), new THREE.Vector3(0.6, 0, 1.0), new THREE.Vector3(0.2, 0, 1.0), new THREE.Vector3(-0.2, 0, 1.0), new THREE.Vector3(-0.6, 0, 1.0), new THREE.Vector3(-1.0, 0, 1.0), new THREE.Vector3(-1.4, 0, 1.0)],
+    [new THREE.Vector3(1.4, 0, 0.6), new THREE.Vector3(1.0, 0, 0.6), new THREE.Vector3(0.6, 0, 0.6), new THREE.Vector3(0.2, 0, 0.6), new THREE.Vector3(-0.2, 0, 0.6), new THREE.Vector3(-0.6, 0, 0.6), new THREE.Vector3(-1.0, 0, 0.6), new THREE.Vector3(-1.4, 0, 0.6)],
+    [new THREE.Vector3(1.4, 0, 0.2), new THREE.Vector3(1.0, 0, 0.2), new THREE.Vector3(0.6, 0, 0.2), new THREE.Vector3(0.2, 0, 0.2), new THREE.Vector3(-0.2, 0, 0.2), new THREE.Vector3(-0.6, 0, 0.2), new THREE.Vector3(-1.0, 0, 0.2), new THREE.Vector3(-1.4, 0, 0.2)],
+    [new THREE.Vector3(1.4, 0, -0.2), new THREE.Vector3(1.0, 0, -0.2), new THREE.Vector3(0.6, 0, -0.2), new THREE.Vector3(0.2, 0, -0.2), new THREE.Vector3(-0.2, 0, -0.2), new THREE.Vector3(-0.6, 0, -0.2), new THREE.Vector3(-1.0, 0, -0.2), new THREE.Vector3(-1.4, 0, -0.2)],
+    [new THREE.Vector3(1.4, 0, -0.6), new THREE.Vector3(1.0, 0, -0.6), new THREE.Vector3(0.6, 0, -0.6), new THREE.Vector3(0.2, 0, -0.6), new THREE.Vector3(-0.2, 0, -0.6), new THREE.Vector3(-0.6, 0, -0.6), new THREE.Vector3(-1.0, 0, -0.6), new THREE.Vector3(-1.4, 0, -0.6)],
+    [new THREE.Vector3(1.4, 0, -1.0), new THREE.Vector3(1.0, 0, -1.0), new THREE.Vector3(0.6, 0, -1.0), new THREE.Vector3(0.2, 0, -1.0), new THREE.Vector3(-0.2, 0, -1.0), new THREE.Vector3(-0.6, 0, -1.0), new THREE.Vector3(-1.0, 0, -1.0), new THREE.Vector3(-1.4, 0, -1.0)],
+    [new THREE.Vector3(1.4, 0, -1.4), new THREE.Vector3(1.0, 0, -1.4), new THREE.Vector3(0.6, 0, -1.4), new THREE.Vector3(0.2, 0, -1.4), new THREE.Vector3(-0.2, 0, -1.4), new THREE.Vector3(-0.6, 0, -1.4), new THREE.Vector3(-1.0, 0, -1.4), new THREE.Vector3(-1.4, 0, -1.4)],
+];
 
 export function pickPiece(event, raycaster, camera, scene) {
     var mouseX = ( event.clientX / window.innerWidth ) * 2 - 1;
@@ -31,19 +41,8 @@ export function pickPiece(event, raycaster, camera, scene) {
         try {
             pieceClicked.material.color.set(pieceMaterial.getHex())
 
-            // matrixWorld is world coordinates, while matrix is local coordinates. index 12 is x, 13 is y, 14 is z
-            // position.x is local coordinates
-            // example of the pieces x coordinate in the world system
-            // console.log(pieceClicked.matrixWorld[13]);
-            // unfortunately, you can't seem to edit this directly, or our job would be easy. Let me know if you figure it out.
-            // instead, I calculate the transformation in the world space and convert it to local space, then do a local space transformation.
-
-            // world space vector transformation from piece location to new world location
-            var V = new THREE.Vector3(intersects[0].point.x - pieceClicked.matrixWorld.elements[12], 0, intersects[0].point.z - pieceClicked.matrixWorld.elements[14]);
-            
-            // 1 in local space is .25 in world space (at least on my computer. This may not be transferable)
-            pieceClicked.position.x += V.x * 4;
-            pieceClicked.position.z += V.z * 4;
+            var squareC = whichSquare(intersects[0].point);
+            worldVectorTranslate(boardSpaceCoordinates[squareC[0]][squareC[1]], pieceClicked);
  
         }
         catch (e) {
@@ -51,7 +50,86 @@ export function pickPiece(event, raycaster, camera, scene) {
         }
     }
 }
+// translates piece (a scene object) to newLoc (world coordinate Vector3)
+// newLoc can be the point from a raycast, and piece can be pieceClicked
+////////// Some notes for the sake of the group
+            // matrixWorld is world coordinates, while matrix is local coordinates. index 12 is x, 13 is y, 14 is z
+            // position.x is local coordinates
+            // example of the pieces x coordinate in the world system
+            // console.log(pieceClicked.matrixWorld[13]);
+            // unfortunately, you can't seem to edit this directly, or our job would be easy. Let me know if you figure it out.
+            // instead, I calculate the transformation in the world space and convert it to local space, then do a local space transformation.
+function worldVectorTranslate(newLoc, piece){
+    var V = new THREE.Vector3(newLoc.x - piece.matrixWorld.elements[12], 0, newLoc.z - piece.matrixWorld.elements[14]);
+    // 1 in local space is .25 in world space (at least on my computer. This may not be transferable)
+    piece.position.x += V.x * 4;
+    piece.position.z += V.z * 4;
+}
 
+// returns array location of chess square at location of vector in world space.
+// Returns an array with the column, then row, as different values
+// for example, the square b3 would return [1, 2]
+function whichSquare(V){
+    // how far past the line you have to click
+    var epsilon = .01;
+    var row, col;
+    // check if within bounds of board. If not, return NaN (-1?)
+    if (V.x > 1.65 || V.x < -1.65 || V.z > 1.65 || V.z < -1.65){
+        return [-1. -1];
+    }
+
+    // find column
+    if (V.z > 1.2 + epsilon){
+        col = 0;
+    }
+    else if (V.z > .8 + epsilon){
+        col = 1;
+    }
+    else if (V.z > .4 + epsilon){
+        col = 2;
+    }
+    else if (V.z > 0 + epsilon){
+        col = 3;
+    }
+    else if (V.z > -.4 + epsilon){
+        col = 4;
+    }
+    else if (V.z > -.8 + epsilon){
+        col = 5;
+    }
+    else if (V.z > -1.2 + epsilon){
+        col = 6;
+    }
+    else 
+        col = 7;
+
+    // find row
+    if (V.x > 1.2 + epsilon){
+        row = 0;
+    }
+    else if (V.x > .8 + epsilon){
+        row = 1;
+    }
+    else if (V.x > .4 + epsilon){
+        row = 2;
+    }
+    else if (V.x > 0 + epsilon){
+        row = 3;
+    }
+    else if (V.x > -.4 + epsilon){
+        row = 4;
+    }
+    else if (V.x > -.8 + epsilon){
+        row = 5;
+    }
+    else if (V.x > -1.2 + epsilon){
+        row = 6;
+    }
+    else 
+        row = 7;
+
+    return [col, row];
+}
 export function resetSelection() {
     firstClick = !firstClick
     pieceClicked.material.color.set(pieceMaterial.getHex())

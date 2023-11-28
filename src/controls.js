c752dd640350663449e83c62fdd90bdfa9512794
Zachary Rose <zacerose@ut.utm.edu>
2023-11-27 22:29:34 -0600

    Added two new functions for moving pieces on the board, as well as a function for moving each piece to its appropriate location based on the piecesArray

--- src/controls.js
+++ src/controls.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { pieceArray } from './initialization';
+import {pieceArray } from './initialization';
 import { checkValidMoves, findPieceSide, Turn} from './util';
 
 const allowedObjects = ['Pawn006', 'Rook004', 'Knight', 'bishop002', 'queeen', 'king000', 'Pawn005', 'Rook001', 'Knight003', 'bishop003', 'queeen001', 'king001']
@@ -8,6 +8,9 @@ const allowedObjects = ['Pawn006', 'Rook004', 'Knight', 'bishop002', 'queeen', '
 
 var firstClick = false;
 var pieceClicked = NaN;
+var pieceClickedSquare = NaN;
+var firstMoveOfGame = true;
+let defaultBoardState = [];
 var col, row;
 var pieceMaterial = 0;
 var boardSpaceCoordinates = [
@@ -24,6 +27,11 @@ var turn = Turn.WhiteTurn;
 var validMoves = []
 
 export function pickPiece(event, raycaster, camera, scene) {
+    // start of the game version of the board
+    if (firstMoveOfGame){
+        defaultBoardState = pieceArray.slice();
+        firstMoveOfGame = false;
+    }
     var mouseX = ( event.clientX / window.innerWidth ) * 2 - 1;
     var mouseY = - ( event.clientY / window.innerHeight ) * 2 + 1;   
 
@@ -34,8 +42,8 @@ export function pickPiece(event, raycaster, camera, scene) {
         if(findPieceSide(intersects[0].object) != undefined && findPieceSide(intersects[0].object) == turn) {
             firstClick = true
             checkValidMoves(intersects[0].object, validMoves)
-            console.log(validMoves)
-            console.log(intersects[0].object); // Shows what object was clicked in console for debugging purposes
+            //console.log(validMoves)
+            //console.log(intersects[0].object); // Shows what object was clicked in console for debugging purposes
             if (allowedObjects.includes(intersects[0].object.name)) {  // checks to see if the intersected object is a chess piece
                 try {
                     pieceMaterial = intersects[0].object.material.color.clone();
@@ -53,27 +61,35 @@ export function pickPiece(event, raycaster, camera, scene) {
         firstClick = false;
         raycaster.setFromCamera( new THREE.Vector2(mouseX, mouseY), camera ); 
         var intersects = raycaster.intersectObjects(scene.children, true);
-        console.log(intersects[0].object); // Shows what object was clicked in console for debugging purposes
+        //console.log(intersects[0].object); // Shows what object was clicked in console for debugging purposes
         var squareC = whichSquare(intersects[0].point);
-        if(pieceArray[(8 * squareC[1]) + squareC[0]] == undefined) {            
+         /* if(pieceArray[(8 * squareC[1]) + squareC[0]] == undefined) {            
             pieceArray[(8 * squareC[1]) + squareC[0]] = pieceClicked
             pieceArray[(8 * row) + col] = undefined
             worldVectorTranslate(boardSpaceCoordinates[squareC[0]][squareC[1]], pieceClicked);
-            console.log(pieceArray)
             swapTurn()
         }
         else if (pieceArray[(8 * squareC[1]) + squareC[0]] != undefined && findPieceSide(pieceArray[(8 * squareC[1]) + squareC[0]]) != findPieceSide(pieceClicked)) {
             var temp = pieceArray[(8 * squareC[1]) + squareC[0]];
-            console.log(findPieceSide(pieceClicked))
-            console.log(findPieceSide(temp))
+            //console.log(findPieceSide(pieceClicked))
+            //console.log(findPieceSide(temp))
+            // piece is moved to new spot in pieceArray
             pieceArray[(8 * squareC[1]) + squareC[0]] = pieceClicked;
+            // Remove duplicate copy of now moved piece
+            pieceArray[(8 * row) + col] = undefined;
             scene.remove(temp.parent);
+
+            // remove taken piece from piece array
+            //pieceArray[(8 * squareC[1]) + squareC[0]] = undefined;
             worldVectorTranslate(boardSpaceCoordinates[squareC[0]][squareC[1]], pieceClicked)
             console.log(temp.material)
             swapTurn()
-        }
+        }  */
+        // If you'd like to switch to previous implementation, uncomment block above and comment this line below
+        makeMoveFromPiece(pieceClicked, squareC, row, col, scene);
+
         pieceClicked.material.color.set(pieceMaterial.getHex())
-        console.log(pieceArray)
+        //console.log(pieceArray)
         pieceClicked = NaN; // unselects piece after move
     }
 }
@@ -82,6 +98,53 @@ function swapTurn() {
     turn = -turn
 }
 
+// logic for making a move seperated into its own function
+// Makes relevant updates to the pieceArray
+// squareC is the board location where the piece will move
+// pieceClicked is the currently selected piece that will be moving
+function makeMoveFromPiece(pieceClicked, squareC, row, col, scene){
+    console.log(pieceClicked);
+    if(pieceArray[(8 * squareC[1]) + squareC[0]] == undefined) {            
+        pieceArray[(8 * squareC[1]) + squareC[0]] = pieceClicked
+        pieceArray[(8 * row) + col] = undefined
+
+        // move pieces where needed from updated piecesArray
+        renderFromBoardStateArray();
+
+        swapTurn()
+    }
+    else if (pieceArray[(8 * squareC[1]) + squareC[0]] != undefined && findPieceSide(pieceArray[(8 * squareC[1]) + squareC[0]]) != findPieceSide(pieceClicked)) {
+        var temp = pieceArray[(8 * squareC[1]) + squareC[0]];
+
+        // piece is moved to new spot in pieceArray
+        pieceArray[(8 * squareC[1]) + squareC[0]] = pieceClicked;
+
+        // Remove duplicate copy of now moved piece
+        pieceArray[(8 * row) + col] = undefined;
+        scene.remove(temp.parent);
+
+        // remove taken piece from piece array
+        //pieceArray[(8 * squareC[1]) + squareC[0]] = undefined;
+
+        // move pieces where needed from updated piecesArray
+        renderFromBoardStateArray();
+
+        swapTurn()
+    }
+}
+// Makes a move from one space to another. Doesn't check if it's legal. Updates pieceArray accordingly
+// this one is mainly for stockfish
+// make sure not to try and use this simultaneously with the other movement function
+function makeMoveFromBoard(fromSpace, toSpace, scene){
+    // check if there is a piece at the location
+    let pieceClicked = pieceArray[(8 * fromSpace[1]) + fromSpace[0]];
+    //console.log(pieceClicked);
+    if (pieceClicked != undefined){
+        makeMoveFromPiece(pieceClicked, toSpace, fromSpace[1], fromSpace[0], scene);
+    }
+    else {//console.log("piecenotfound"); console.log(pieceClicked);
+    }
+}
 // translates piece (a scene object) to newLoc (world coordinate Vector3)
 // newLoc can be the point from a raycast, and piece can be pieceClicked
 ////////// Some notes for the sake of the group
@@ -98,11 +161,20 @@ function worldVectorTranslate(newLoc, piece){
     piece.position.z += V.z * 4;
 }
 
+// moves each piece to their appropriate position from the pieceArray (try to use this instead of calling worldVectorTranslate directly)
+function renderFromBoardStateArray(){
+    for (var i = 0; i < pieceArray.length; i++){
+        if (pieceArray[i] != undefined){
+            var space = [i % 8, Math.floor(i / 8)];
+            worldVectorTranslate(boardSpaceCoordinates[space[0]][space[1]], pieceArray[i]);
+        }
+    }
+}
 // returns array location of chess square at location of vector in world space.
 // Returns an array with the column, then row, as different values
 // for example, the square b3 would return [1, 2]
 export function whichSquare(V){
-    // how far past the line you have to click
+    // how far within the square you have to click
     var epsilon = .01;
     var row, col;
     // check if within bounds of board. If not, return NaN (-1?)
